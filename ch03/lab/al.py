aacc9dbd0b9df4d7a81d00a9dc86884bc77c1fd6
aayesha khan <akhan90@binghamton.edu>
2023-02-20 23:20:18 -0500

    lab2

---
+++ ch03/lab/al.py
@@ -0,0 +1,25 @@
+import pygame
+import math
+
+pygame.init()
+screen = pygame.display.set_mode()
+
+screen.fill ("blue")
+pygame.display.flip()
+pygame.time.wait(1000)
+
+#points = []
+#num_sides = 
+#side_length =
+#xpos = 
+#ypos = 
+
+pygame.draw.polygon (screen, "black", [[100,100], [150,300], [100,200]])
+pygame.time.wait (1000)
+pygame.draw.polygon (screen, "black", [[700,-200], [550,200], [500,100], [750, 300]])
+#for _ in range (3):
+#    angle = 360/num_sides
+
+
+pygame.display.flip()
+pygame.time.wait (1000)
--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -11,16 +11,52 @@ michelangelo.color('orange')
 leonardo.color('blue')
 michelangelo.shape('turtle')
 leonardo.shape('turtle')
-
+leonardo.speed(1)
+michelangelo.speed(1)
 michelangelo.up() # 4. Pick up the pen so we don’t get lines
 leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. Your PART A code goes here
+turtle.delay (20)
+leonardo.forward (100)
+michelangelo.forward (50)
+turtle.ontimer (20)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
+for _ in range (3):
+    leonardo.forward (random.randrange(1,10))
+    michelangelo.forward (random.randrange(1,10))
+    
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+window.exitonclick()
 
 # PART B - complete part B here
+import pygame
+import math
+
+pygame.init()
+screen = pygame.display.set_mode()
+
+screen.fill ("blue")
+pygame.display.flip()
+pygame.time.wait(1000)
+
+#points = []
+#num_sides = 
+#side_length =
+#xpos = 
+#ypos = 
+
+pygame.draw.polygon (screen, "black", [[100,100], [150,300], [100,200]])
+pygame.time.wait (1000)
+pygame.draw.polygon (screen, "black", [[700,-200], [550,200], [500,100], [750, 300]])
+#for _ in range (3):
+#    angle = 360/num_sides
 
 
-window.exitonclick()
+pygame.display.flip()
+pygame.time.wait (1000)
