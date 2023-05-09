58cbd330d1f7a1b6a841a81ad15376467d4d62ab
aayesha khan <akhan90@binghamton.edu>
2023-05-08 22:30:00 -0400

    final

--- ch10/final/main.py
+++ ch10/final/main.py
@@ -7,6 +7,6 @@ activity_api = BoredAPI()
 joke = joke_api.get()
 activity = activity_api.get()
 
-print(f"{joke} Was that a bad dad joke? Here is an activity to do to make up for it..hopefully:  {activity}?")
+print(f"{joke} Was that a bad dad joke? Don't have a dad? Here's an activity to do to make up for it..hopefully:  {activity}?")
 
 
